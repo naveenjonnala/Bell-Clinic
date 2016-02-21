1ac28f6c19c449ebc8825d24f16ad41a04e37534
Naveen Reddy <naveenreddy.j1808@gmail.com>
2016-02-21 18:52:21 -0500

    Done with layout

--- js/app.js
+++ js/app.js
@@ -65,3 +65,72 @@ function Validate() {
         }
         return true;
     }
+
+$(function() {
+    var availableTags = [
+      "Nasal congestion",
+      "sneezing",
+      "sore throat",
+      "runny nose",
+      "cough",
+      "postnal drip",
+      "fever",
+      "indigestion",
+      "headache",
+      "body aches",
+      "backpain",
+      "vomiting",
+      "toothache",
+      "heartburn",
+      "arm pain",
+      "sweating",
+      "jaw pain",
+      "fatigue",
+      "testicular pain",
+      "stomach pain",
+      "nausea",
+      "chest pain"
+    ];
+    function split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+ 
+    $( "#tags" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        minLength: 0,
+        source: function( request, response ) {
+          // delegate back to autocomplete, but extract the last term
+          response( $.ui.autocomplete.filter(
+            availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
+  });
+
+//------------------------------------------------------------------------------------------
+
+
